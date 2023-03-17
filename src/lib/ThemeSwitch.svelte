391bc732b48d834795f7b5df302264161281da2f
Dennis de Vries <dennis.de.vries@divotion.com>
2023-03-17 15:41:19 +0100

    Add `<ThemeSwitch />` component

---
+++ src/lib/ThemeSwitch.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	import { browser } from '$app/environment';
+
+	let darkMode = true;
+
+	function handleSwitchDarkMode() {
+		darkMode = !darkMode;
+
+		localStorage.setItem('theme', darkMode ? 'dark' : 'light');
+
+		darkMode
+			? document.documentElement.classList.add('dark')
+			: document.documentElement.classList.remove('dark');
+	}
+
+	if (browser) {
+		if (
+			localStorage.theme === 'dark' ||
+			(!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)
+		) {
+			darkMode = true;
+		} else {
+			darkMode = false;
+		}
+	}
+</script>
+
+<label>
+	<input checked={darkMode} on:click={handleSwitchDarkMode} type="checkbox" id="theme-toggle" />
+	<span>Dark mode</span>
+</label>
