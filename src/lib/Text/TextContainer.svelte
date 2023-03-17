0f65d17c18c1103548861ffef95bc47d783f78a1
Dennis de Vries <dennis.de.vries@divotion.com>
2023-03-17 17:31:03 +0100

    Add `<TextContainer />` component

---
+++ src/lib/Text/TextContainer.svelte
@@ -0,0 +1,3 @@
+<div class="bg-white dark:bg-bts-black opacity-70 p-4">
+	<slot />
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,44 @@
 <script lang="ts">
 	import StartButton from '$lib/Button/StartButton.svelte';
+	import TextContainer from '$lib/Text/TextContainer.svelte';
 </script>
 
 <section>
+	<TextContainer>
+		<p>
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam pulvinar magna nisi, vitae
+			ornare quam interdum in. Nunc finibus tristique ex, ut mollis sapien. Nullam rhoncus varius
+			tortor vel sodales. Nullam lacinia pharetra purus quis efficitur. Sed id nisi sapien. Nam quis
+			nibh non nisl aliquet tincidunt a eget turpis. Curabitur et dolor est. Proin eros libero,
+			ornare at tristique eget, mattis id nulla. Duis ligula dui, consequat id ex vitae, rhoncus
+			pharetra nisl. Curabitur semper egestas velit at iaculis. Praesent non augue ullamcorper,
+			eleifend nisi vitae, pulvinar nibh. Aliquam eleifend nisi eget libero imperdiet efficitur.
+			Vestibulum euismod sagittis neque pretium blandit. Nulla laoreet massa sit amet nulla
+			pharetra, vel hendrerit augue aliquet. Proin rhoncus non mauris sit amet blandit. Quisque eget
+			pretium turpis. Maecenas ut tristique enim. Fusce libero metus, ultrices a viverra quis,
+			vehicula at nisl. Fusce ut gravida est. Phasellus libero nisi, suscipit eget lorem nec,
+			pharetra consequat odio. Sed eget ultricies dolor, quis viverra nisl. Donec finibus nibh
+			purus, sit amet luctus nunc gravida quis. Suspendisse lobortis nibh ex, sed rutrum libero
+			sodales at. Quisque leo nulla, sagittis nec odio aliquam, cursus fermentum urna. Aliquam lacus
+			erat, dapibus id eros a, fringilla laoreet orci. Sed magna tortor, euismod ac scelerisque nec,
+			cursus in ex. Sed suscipit nunc a enim vulputate posuere. Suspendisse libero massa, semper
+			vitae justo in, interdum faucibus quam. Sed eu placerat tellus. Etiam non accumsan tellus.
+			Proin congue arcu faucibus risus tincidunt eleifend. Nam fringilla lacinia placerat. Etiam leo
+			dolor, dapibus non commodo quis, faucibus efficitur elit. Nam placerat nisl lorem, ut
+			consectetur nisi pretium eu. Nunc odio nibh, sagittis nec lectus eget, tristique laoreet
+			turpis. Sed sagittis velit id erat malesuada, in interdum nisl porttitor. Donec fermentum
+			elementum orci, in vestibulum eros fringilla a. Vestibulum ut dui mauris. Interdum et
+			malesuada fames ac ante ipsum primis in faucibus. Praesent nec varius risus, vitae rhoncus mi.
+			Duis non ligula vitae dui vestibulum lobortis fringilla non quam. Vestibulum ante ipsum primis
+			in faucibus orci luctus et ultrices posuere cubilia curae; Phasellus consectetur venenatis
+			tellus vitae imperdiet. Pellentesque tincidunt sodales neque ac tincidunt. Duis tristique id
+			enim a sollicitudin. Duis tellus ex, volutpat ut velit eu, tincidunt fringilla mauris.
+			Suspendisse nec eleifend eros, ut euismod augue. Pellentesque gravida lacus et venenatis
+			pretium. Nunc luctus pretium fermentum. Etiam dapibus dolor quis porttitor feugiat. Integer eu
+			quam eget massa rutrum fringilla a a arcu. Donec nunc mauris, scelerisque eget nisi at,
+			scelerisque accumsan neque. Quisque vitae blandit nunc. Sed vel mollis ligula. Aenean eu
+			sagittis ipsum. Proin leo augue, sagittis ac felis sit amet, elementum tristique mauris.
+		</p>
+	</TextContainer>
 	<StartButton />
 </section>
