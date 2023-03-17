6bfcdc133fc83e7e6115e9dc6d3608236615057b
Dennis de Vries <dennis.de.vries@divotion.com>
2023-03-17 15:58:24 +0100

    W.I.P.

---
+++ src/lib/Form.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	const submitHandler = function (e: SubmitEvent) {
+		e.preventDefault();
+
+		const formData = new FormData(e.target as HTMLFormElement);
+
+		console.log({ formData });
+	};
+</script>
+
+<form on:submit={submitHandler}>
+	<button type="submit">submit</button>
+</form>
