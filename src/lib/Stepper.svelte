e3594db921de1c2f407f7ab3681742813ef59bf0
Dennis de Vries <dennis.de.vries@divotion.com>
2023-03-17 22:28:12 +0100

    Correct index

--- src/lib/Stepper.svelte
+++ src/lib/Stepper.svelte
@@ -1,20 +1,38 @@
 <script lang="ts">
-	import { currentStep } from "../store";
-	import StepButton from "./StepButton.svelte";
+	import { currentStep } from '../store';
+	import StepButton from './StepButton.svelte';
 
-	let localStep: number;  	
-	currentStep.subscribe((value) => (localStep = value))
+	let localStep: number;
+	currentStep.subscribe((value) => (localStep = value));
 
 	function handleClick(newStep: number) {
-	 
-		currentStep.update(n => newStep)
+		currentStep.update((n) => newStep);
 	}
- 
 </script>
 
 <div class="mx-auto bg-bts-gray rounded-md p-2">
-	<StepButton active={1 === localStep} on:click={() => {handleClick(1)}}>1</StepButton>
-	<StepButton active={2 === localStep} on:click={() => {handleClick(2)}}>2</StepButton>
-	<StepButton active={3 === localStep} on:click={() => {handleClick(3)}}>3</StepButton>
-	<StepButton active={4 === localStep} on:click={() => {handleClick(4)}}>4</StepButton>
+	<StepButton
+		active={0 === localStep}
+		on:click={() => {
+			handleClick(0);
+		}}>1</StepButton
+	>
+	<StepButton
+		active={1 === localStep}
+		on:click={() => {
+			handleClick(1);
+		}}>2</StepButton
+	>
+	<StepButton
+		active={2 === localStep}
+		on:click={() => {
+			handleClick(2);
+		}}>3</StepButton
+	>
+	<StepButton
+		active={3 === localStep}
+		on:click={() => {
+			handleClick(3);
+		}}>4</StepButton
+	>
 </div>
