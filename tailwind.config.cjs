4f5a72866fbdaf4507aa86dddc1356216f187f5c
Dennis de Vries <dennis.de.vries@divotion.com>
2023-03-17 15:49:00 +0100

    Add custom theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,15 @@ module.exports = {
 	darkMode: 'class',
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
-		extend: {}
+		extend: {
+			colors: {
+				'bts-gray': '#565659',
+				'bts-light-gray': '#8F92A6',
+				'bts-brown': '#8C613B',
+				'bts-light-brown': '#BF9B8E',
+				'bts-black': '#0D0D0D'
+			}
+		}
 	},
 	plugins: []
 };
