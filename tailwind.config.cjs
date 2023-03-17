2dd41d433cfb32ca3e57622e480c3e5f166ef878
jpgdiv <peter@divotion.com>
2023-03-17 17:55:40 +0100

    bg

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -11,7 +11,12 @@ module.exports = {
 				'bts-light-brown': '#BF9B8E',
 				'bts-dark-brown': '#552413',
 				'bts-black': '#0D0D0D'
-			}
+			},
+			backgroundImage: {
+				'hero-light': "url('/hero-light.jpg')",
+				'hero-dark': "url('/hero-dark.jpg')",
+				'hero-contrast': "url('/hero-contrast.jpg')",
+			  }
 		}
 	},
 	plugins: [require('@tailwindcss/typography')]
