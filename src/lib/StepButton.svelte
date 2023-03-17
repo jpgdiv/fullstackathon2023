04f9a1bc3510792d74d14dd54a3af69e58925f31
jpgdiv <peter@divotion.com>
2023-03-17 21:35:46 +0100

    Stepper

---
+++ src/lib/StepButton.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+
+export let active: Boolean;
+
+
+</script>
+
+<button on:click class={`${active? "bg-bts-light-brown" : "bg-bts-light-gray"} px-6 py-4 mx-4 rounded-full`}>
+	<slot />
+</button>
---
+++ src/lib/Stepper.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	// import { currentStep } from "../store";
+	import StepButton from "./StepButton.svelte";
+	// currentStep.subscribe((value) => (localStep = value))
+	let localStep: number; 
+	
+	function handleClick(newStep: number) {
+		localStep = newStep
+	}
+ 
+</script>
+
+<div class="mx-auto bg-bts-gray rounded-md p-2">
+	<StepButton active={1 === localStep} on:click={() => {handleClick(1)}}>1</StepButton>
+	<StepButton active={2 === localStep} on:click={() => {handleClick(2)}}>2</StepButton>
+	<StepButton active={3 === localStep} on:click={() => {handleClick(3)}}>3</StepButton>
+	<StepButton active={4 === localStep} on:click={() => {handleClick(4)}}>4</StepButton>
+</div>
