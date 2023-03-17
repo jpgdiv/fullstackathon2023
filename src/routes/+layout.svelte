71b3e8ecd8f47ec11fc8b4ec4bf519538a9ce59d
jpgdiv <peter@divotion.com>
2023-03-18 00:54:00 +0100

    contrast mode

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,5 +6,14 @@
 
 <Header />
 <div class="container mx-auto p-4">
+	<button on:click={() => {
+
+		const body = document.getElementsByTagName('body');
+		body[0].classList.remove("normal")
+		body[0].classList.add("invert")
+		body[0].classList.add("bg-contrast")
+
+
+	}}>contast mode</button>
 	<slot />
 </div>
