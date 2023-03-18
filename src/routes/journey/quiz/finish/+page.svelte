b4f883b28c9c6fa6c5bcf955cdaa37b66080163f
Dennis de Vries <dennis.de.vries@divotion.com>
2023-03-18 10:22:47 +0100

    Finish page

--- src/routes/journey/quiz/finish/+page.svelte
+++ src/routes/journey/quiz/finish/+page.svelte
@@ -1,4 +1,12 @@
 <script lang="ts">
 </script>
 
-<div>derp</div>
+<section class="grid grid-cols-1 gap-4 text-center">
+	<h2 class="prose-2xl">Finish! Sleep well</h2>
+
+	<img
+		src="https://media2.giphy.com/media/KVDKSkxb89xaagnfft/giphy.gif?cid=ecf05e47x9l9k3z4iqhs0mg2h22wfk8ve5lwb3yqpxgdk459&rid=giphy.gif&ct=g"
+		alt=""
+		class="justify-self-center"
+	/>
+</section>
