e9df5d5c00c3181a3c84fc52a30d758bc684ea20
Dennis de Vries <dennis.de.vries@divotion.com>
2023-03-18 10:09:23 +0100

    Remove contrast mode test button

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,14 +6,5 @@
 
 <Header />
 <div class="container mx-auto p-4">
-	<button
-		on:click={() => {
-			const body = document.getElementsByTagName('body');
-			const main = document.getElementsByTagName('main');
-			body[0].classList.remove('normal');
-			main[0].classList.add('invert');
-			body[0].classList.add('backdrop-invert');
-		}}>contast mode</button
-	>
 	<slot />
 </div>
